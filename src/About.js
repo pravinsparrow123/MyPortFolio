908e71cbbe6984be35e3334dedceac67a2614d38
Pravin Kumar <pravinpk58585@gmail.com>
2024-07-29 23:01:11 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      {/* Add content for your about page here */}
+    </div>
+  );
+}
+
+export default About;
---
+++ src/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>The page you are looking for doesn't exist.</p>
+      {/* Add any additional content or links here */}
+    </div>
+  );
+}
+
+export default NotFound;
